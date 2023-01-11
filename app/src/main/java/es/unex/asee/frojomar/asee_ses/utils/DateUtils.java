81bedaf53b4a5872d8a3752a9b7c3371cc2df0b5
Carlos <cacasquer@alumnos.unex.es>
2023-01-11 18:23:38 +0100

    //Implementación CU18-DS

--- app/src/main/java/es/unex/asee/frojomar/asee_ses/utils/DateUtils.java
+++ app/src/main/java/es/unex/asee/frojomar/asee_ses/utils/DateUtils.java
@@ -14,6 +14,7 @@ import java.util.TimeZone;
 public class DateUtils {
 
     //Simulación implementación del caso de uso 18 - Rol: Arquitecto Software
+    //Simulación implementación del caso de uso 18 - Rol: Desarrollador Senior
     //private static final String FORMAT_DATE="dd-MMM-yyyy,hh:mm aa";
     private static final String FORMAT_TIME = "HH:mm:ss:SSS";
     private static final String FORMAT_DATE = "d-MMM-yyyy,HH:mm:ss aa";
